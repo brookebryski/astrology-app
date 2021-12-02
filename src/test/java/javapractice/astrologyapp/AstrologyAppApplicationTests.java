233647cb199575a3a7454a0168fde432656ad97f
BrookeBryski <brookeabryski@gmail.com>
2021-12-02 11:37:36 -0600

    initial commit

---
+++ src/test/java/javapractice/astrologyapp/AstrologyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package javapractice.astrologyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstrologyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
